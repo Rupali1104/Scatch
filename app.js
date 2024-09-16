8a1fa257083255f345aec5fa9b38985f360c3175
Rupali R <rupaliii739@gmail.com>
2024-09-16 22:44:29 +0530

    segregated routers and connected them

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
-
 const path = require("path");
 const cookieParser = require("cookie-parser");
+const ownersRouter = require("./routes/ownersRouter");
+const productsRouter = require("./routes/productsRouter");
+const usersRouter = require("./routes/usersRouter");
+
+const db = require("./config/mongoose-connection");
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
@@ -11,8 +15,8 @@ app.use(cookieParser());
 
 app.set("view engine","ejs");
 
-app.get('/',(req,res)=>{
-    res.send("heyy");
-})
+app.use("/owners", ownersRouter);
+app.use("/users", usersRouter);
+app.use("/products", productsRouter);
 
 app.listen(3000);
